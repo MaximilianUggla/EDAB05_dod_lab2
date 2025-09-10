cad352d44ea772c3fc8ffc773b42460b23567200
MaximilianUggla <maxi.uggla@icloud.com>
2025-09-10 14:06:59 +0200

    Secound clone says, hi

--- HelloWorld.scala
+++ HelloWorld.scala
@@ -1,5 +1,5 @@
 object HelloWorld {
     def main(args: Array[String]): Unit = {
-        println("Hello, negation world!!")
+        println("Hello, negation world, this is secondary clone!!")
     }
 }
